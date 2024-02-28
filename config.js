c44e2c81229840092439c775a9a62972eab922e2
Diana Botchway Frimpong <124820831+Di-anaBF@users.noreply.github.com>
2024-02-28 11:15:55 +0000

    Updated_Storymap

--- config.js
+++ config.js
@@ -7,21 +7,21 @@ var config = {
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: false,
-    theme: 'light',
+    theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Example 101',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Diana Botchway Frimpong',
+    title: 'THE BEAUTY OF A PINE',
+    subtitle: 'The true meaning of life is to love and plant trees. Learn with me.',
+    byline: 'By Diana Botchway Frimpong',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
-            alignment: 'left', //options are left, right, center, full
+            alignment: 'center', //options are left, right, center, full
             hidden: false,
-            title: 'Display Title',
-            image: 'images/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            title: 'LOBLOLLY PINE',
+            image: 'images/Loblolly.png',
+            description: 'Inspired by the strength, power, beauty and incredible role they play in our world today, these are series of information about the Loblolly Pine The loblolly pine is also known as the bull pine, Arkansas Pine or the North Carolina Pine. It is an evergreen tree from the pinaceae family. It is also known to be one the fastest growing tree in the south. In the space of 20 years it can grow as high as 50 feet. It has the scientific name Pinus taeda.',
             //text for description is html- can rely on that for extra formatting
             location: {
                 center: [-122.418398, 37.759483],
@@ -50,9 +50,9 @@ var config = {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
+            title: 'CHARACTERISTICS OF THE LOBLOLLY',
             image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Identifying a loblolly pine in the midst other pines like the slash pine and long leaf pine could be an herculean task. Be guided by these characteristics and brief recognition factors of the loblolly pine. Swipe right.',
             location: {
                 center: [-77.020636, 38.886900],
                 zoom: 8.5,
